c453e7ced95f8f8f0a265e978c806b6aab485ac4
harish boppana <boppanaharish7475@gmail.com>
2026-02-15 18:58:08 +0530

    aws-console instance creation

--- aws-instances.sh
+++ aws-instances.sh
@@ -38,17 +38,17 @@ do
         "Comment": "Updating record",
         "Changes": [
             {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$Record_name'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
-                {
-                    "Value": '"$IP"'
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$Record_name'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                    {
+                        "Value": '"$IP"'
+                    }
+                    ]
                 }
-                ]
-            }
             }
         ]
     }'
