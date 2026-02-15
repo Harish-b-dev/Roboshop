de5ace003e0d648b5c12c9b1750534fb187d86af
harish boppana <boppanaharish7475@gmail.com>
2026-02-15 18:15:43 +0530

    aws-console instance creation

--- aws-instances.sh
+++ aws-instances.sh
@@ -1,32 +1,32 @@
 #!/bin/shell
 
 AMI_ID="ami-0220d79f3f480ecf5"
-SECURITY_ID=sg-0de4bc98ac346b345
+SECURITY_ID="sg-0de4bc98ac346b345"
 
 
 for instance in $@
 do
-    INSTANCE_ID=$(aws ec2 run-instances \ 
-        --image-id $AMI_ID \
-        --count 1 \
-        --instance-type t3.micro \
-        --security-group-ids $SECURITY_ID \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query 'Instances[0].InstanceId' \
-        --output text)
+    INSTANCE_ID=$( aws ec2 run-instances \ 
+    --image-id $AMI_ID \
+    --count 1 \
+    --instance-type t3.micro \
+    --security-group-ids $SECURITY_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Instances[0].InstanceId' \
+    --output text )
     
-    if [$instance == "frontend" ]; then
-        ID=$(aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[0].Instances[0].PublicIpAddress' \
-            --output text)
+    if [ $instance == "frontend" ]; then
+        ID=$( aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[0].Instances[0].PublicIpAddress' \
+        --output text )
         
         echo "public id :: $ID"
     else
-        ID=$(aws ec2 describe-instances \
-            --instance-ids "$INSTANCE_ID" \
-            --query 'Reservations[0].Instances[0].privateIpAddress' \
-            --output text)
+        ID=$( aws ec2 describe-instances \
+        --instance-ids "$INSTANCE_ID" \
+        --query 'Reservations[0].Instances[0].privateIpAddress' \
+        --output text )
 
         echo "public id :: $ID"
     
