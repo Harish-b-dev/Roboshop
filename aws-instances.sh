afd6600fcad64a0ec73e2cb6cc4ccb099eb35e11
harish boppana <boppanaharish7475@gmail.com>
2026-02-15 19:04:44 +0530

    aws-console instance creation

--- aws-instances.sh
+++ aws-instances.sh
@@ -32,7 +32,7 @@ do
     fi
 
     aws route53 change-resource-record-sets \
-    --hosted-zone-id $ZONE_ID \
+    --hosted-zone-id $HOST_ID \
     --change-batch '
     {
         "Comment": "Updating record",
