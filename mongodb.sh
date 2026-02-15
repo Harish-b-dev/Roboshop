09bf875bd4447233b38d0820e0c5935cbb665b81
harish boppana <boppanaharish7475@gmail.com>
2026-02-15 19:31:45 +0530

    aws-console instance creation

---
+++ mongodb.sh
@@ -0,0 +1,50 @@
+#!/bin/shell
+
+user=$(id -u)
+logs_folder="/var/log/roboshop"
+log_file="$logs_folder/$0.log"
+command_type=$1
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+echo -e " command passed ... $B $command_type $N"
+
+sudo mkdir -p $logs_folder
+
+
+if [ $user -ne 0 ]; then 
+     echo -e  "You need $Y sudo access $N to install packages." | tee -a $log_file
+    exit 1
+
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 $N  installation ... failure " | tee -a $log_file
+        exit 1
+    else
+        echo -e "$G $2 $N installation ... success" | tee -a $log_file
+    fi
+}
+
+
+cp mongo.repo /etc/yum/repos.d/mongo.repo
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing ... mongodb"
+
+systemctl enable mongod 
+VALIDATE $? "enabled ... mongodb"
+
+systemctl start mongod
+VALIDATE $? "started ... mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+
+systemctl restart mongod
+VALIDATE $? "started ... mongodb"
