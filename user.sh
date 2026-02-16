b2729e9946c9fde0da177be9e85eee2f2b0302a2
harish boppana <boppanaharish7475@gmail.com>
2026-02-16 13:58:55 +0530

    aws-console instance creation

---
+++ user.sh
@@ -0,0 +1,89 @@
+#!/bin/shell
+
+user=$(id -u)
+logs_folder="/var/log/roboshop"
+log_file="$logs_folder/$0.log"
+command_type=$1
+start_time=$(date +%s)
+Working_dir="/home/ec2-user"
+Mongodb_host="mongodb.learndaws88s.online"
+
+#set -e
+#trap 'echo "there is an error at $LINENO, command :: $BASH_COMMAND"' ERR
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+N="\e[0m"
+
+echo -e " script execution started now ... $B $command_type $N"
+
+sudo mkdir -p $logs_folder
+
+
+if [ $user -ne 0 ]; then 
+     echo -e  "You need $Y sudo access $N to install packages." | tee -a $log_file
+    exit 1
+
+fi
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 $N ... failure " | tee -a $log_file
+        exit 1
+    else
+        echo -e "$G $2 $N ... success" | tee -a $log_file
+    fi
+}
+
+
+dnf module disable nodejs -y &>> $log_file
+VALIDATE $? "nodejs disabled"
+
+dnf module enable nodejs:20 -y &>> $log_file
+VALIDATE $? "nodejs version 20 enabled"
+
+dnf install nodejs -y&>> $log_file
+VALIDATE $? "nodejs installed" | tee -a $log_file
+
+id roboshop &>> $log_file
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop 
+    VALIDATE $? "adding user" &>> $log_file
+else
+    echo "Roboshop user already exists"
+fi
+
+mkdir -p /app 
+
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+cd /app
+
+rm -rf /app/*
+
+unzip /tmp/user.zip
+
+npm install &>> $log_file
+
+cp $Working_dir/Roboshop/user.service /etc/systemd/system/user.service
+
+systemctl daemon-reload
+
+systemctl enable user &>> $log_file
+
+systemctl start user &>> $log_file
+VALIDATE $? "user enabled ... started" | tee -a $log_file
+
+cp $Working_dir/Roboshop/mongo.repo /etc/yum.repos.d/mongo.repo
+
+systemctl restart catalogue &>> $log_file
+VALIDATE $? "catalogue restart" | tee -a $log_file
+
+
+
+end_time=$(date +%s)
+final_time=$(($end_time - $start_time))
+echo "script executed at $final_time"
