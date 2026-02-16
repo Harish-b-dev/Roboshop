8f8d08d292f7b6150fc22d7fee575bb6f2298979
harish boppana <boppanaharish7475@gmail.com>
2026-02-17 03:52:48 +0530

    aws-console instance creation

--- nginx.sh
+++ nginx.sh
@@ -1,52 +1,64 @@
-#!/bin/bash
+#!/bin/shell
 
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
+user=$(id -u)
+logs_folder="/var/log/roboshop"
+log_file="$logs_folder/$0.log"
+command_type=$1
+start_time=$(date +%s)
+Working_dir="/home/ec2-user/Roboshop"
+
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+B="\e[34m"
 N="\e[0m"
-SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.learndaws88s.online
 
-if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+echo -e " script execution started now ... $B $command_type $N"
+
+sudo mkdir -p $logs_folder
+
+
+if [ $user -ne 0 ]; then 
+     echo -e  "You need $Y sudo access $N to install packages." | tee -a $log_file
     exit 1
+
 fi
 
-mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$R $2 $N ... failure " | tee -a $log_file
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$2 ... success" | tee -a $log_file
     fi
 }
 
-dnf module disable nginx -y &>>$LOGS_FILE
-dnf module enable nginx:1.24 -y &>>$LOGS_FILE
-dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Installing Nginx"
 
-systemctl enable nginx  &>>$LOGS_FILE
-systemctl start nginx 
-VALIDATE $? "Enabled and started nginx"
+#dnf module disable nginx -y &>> $log_file
+#dnf module enable nginx:1.24 -y &>> $log_file
+dnf module install nginx:1.24 -y &>> $log_file
+VALIDATE $? "Nginx version 1.24 ... installation"
 
-rm -rf /usr/share/nginx/html/* 
-VALIDATE $? "Remove default content"
+systemctl enable nginx
+VALIDATE $? "Nginx enabled and started" 
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOGS_FILE
-cd /usr/share/nginx/html 
-unzip /tmp/frontend.zip &>>$LOGS_FILE
-VALIDATE $? "Downloaded and unzipped frontend"
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>> $log_file
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>> $log_file
+VALIDATE $? "Nginx page unziped"
 
 rm -rf /etc/nginx/nginx.conf
 
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
-VALIDATE $? "Copied our nginx conf file"
+cp /home/ec2-user/Roboshop/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "nginx.conf updated"
 
-systemctl restart nginx
-VALIDATE $? "Restarted Nginx"
+systemctl start nginx
+VALIDATE $? "Nginx restart"
+
+end_time=$(date +%s)
+final_time=$(($end_time - $start_time))
+echo "script executed at $final_time"
